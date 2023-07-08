ff415013f5082deb270936697d4fff9f47bde11f
Vishal N <git@vishalnandagopal.com>
2023-07-08 21:07:45 +0530

    init commit with basic site, calculation js and bad css

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple website I made because the existing online calculators weren't that good. Design (color scheme only) partially inspired by [Groww](https://groww.in/calculators/fd-calculator) & [fd-calculator.in](https://fd-calculator.in/).
+
+Code licensed under [MIT License](./LICENSE). Copyright (c) 2023 Vishal N
---
+++ fd-calculator.js
@@ -0,0 +1,45 @@
+function getValuesAndCalculate() {
+    const principle = parseFloat(document.getElementById("principle").value);
+    const interest_rate = parseFloat(document.getElementById("interest_rate").value);
+    const time = parseFloat(document.getElementById("time").value);
+    const time_unit = document.getElementById("time-units").value;
+    const compounding = parseInt(document.getElementById("compounding").value);
+    const maturity_amount = calculate(principle, interest_rate, time, time_unit, compounding);
+    document.getElementById("answer-section").style.display = "block";
+    if (isNaN(maturity_amount)) {
+        document.getElementById("answer").innerHTML = "Please enter the correct values in the form.";
+    } else {
+        document.getElementById("answer").innerHTML = `Your total maturity amount is <span>${Math.round(maturity_amount * 100) / 100}</span>.`;
+    }
+}
+
+function calculate(principle, interest_rate, time, time_unit, compounding) {
+    let days = 0;
+    let maturity_amount = principle;
+
+    if (time_unit == "y") {
+        days = time * 365 + Math.floor(time / 4);
+    } else if (time_unit == "m") {
+        days = time * 30 + Math.floor(time / 2);
+    } else if (time_unit == "d") {
+        days = time;
+    }
+    console.log(`₹${principle} invested at ${interest_rate}% for ${days} days / ${time} ${time_unit}. Compouding every ${compounding} day(s)`);
+    for (i = 1; i <= days / compounding; i++) {
+        console.log(i);
+        maturity_amount = calculate_interest(maturity_amount, interest_rate, compounding);
+        console.log(maturity_amount);
+    }
+
+    maturity_amount = calculate_interest(maturity_amount, interest_rate, days % compounding);
+    console.log(maturity_amount);
+    return maturity_amount;
+}
+
+function calculate_interest(principle, interest_rate, days) {
+    // This uses simple interest, not compound.
+    // The compounding formula must be used separately. For example, if for a year, it compounds quarterly, you must call it 4 times by adding the interest after each calculation manually.
+    const interest_amount = principle * (interest_rate / 100);
+    const interest_per_day = interest_amount / 365;
+    return principle + interest_per_day * days;
+}
